07c8fbf62c218dac836e14fe7bb9520ca4f1b59e
davidhwyllie <david.wyllie@ndm.ox.ac.uk>
2018-03-14 18:03:48 +0000

    do not repeat existing kraken analysis

--- pipeline/testdata/run_kraken.sh
+++ pipeline/testdata/run_kraken.sh
@@ -16,23 +16,30 @@ do
     if [ -d $GUID ]; then
         cd ${GUID}
         INPUTFILE=${GUID}.fq
-	if [ -f ${INPUTFILE}.gz ]; then
-		echo 'fastq.gz read file found; unzipping'
-                gunzip ${INPUTFILE}.gz --to-stdout > ${INPUTFILE}
-                ${KRAKENSRCDIR}kraken --threads 1 \
+        TARGETFILE=${GUID}.kraken.report.txt
+        if [ -f ${TARGETFILE} ]; then
+                echo 'Skipping; analysis already done'
+        else
+
+		if [ -f ${INPUTFILE}.gz ]; then
+			echo 'fastq.gz read file found; unzipping'
+                	gunzip ${INPUTFILE}.gz --to-stdout > ${INPUTFILE}
+	                ${KRAKENSRCDIR}kraken --threads 1 \
 --preload --db $KRAKENDB \
 --fastq-input $INPUTFILE |
 kraken-filter --db \
 $KRAKENDB --threshold 0.05 > ${GUID}.kraken.txt
 
-kraken-report --db $KRAKENDB \
-${GUID}.kraken.txt > ${GUID}.kraken.report.txt
-                rm ${INPUTFILE}
+			kraken-report --db $KRAKENDB \
+${GUID}.kraken.txt > ${TARGETFILE}
+			# remove the temporary decompressed file
+                	rm -f ${INPUTFILE}
 
-	else
-		echo $TARGETFILE not found
-                exit 1
-        fi
+		else
+			echo ${INPUTFILE}.gz not found
+                	exit 1
+	        fi
+        fi	
     else
 		echo Skipping $GUID as not a directory
     fi

6eead1f96aa20aa848ac53214ba6d0b76519f2bb
davidhwyllie <david.wyllie@ndm.ox.ac.uk>
2018-03-14 18:44:13 +0000

    Mapping script with stampy

--- pipeline/testdata/map_with_stampy.sh
+++ pipeline/testdata/map_with_stampy.sh
@@ -1,9 +1,8 @@
-#!/bin/sh
+!/bin/sh
 # start in testdata;
 cd ../testdata
 
 # construct paths to executables relative to the pipeline directory
-BOWTIE=software/bowtie-1.2.2-linux-x86_64/
 STAMPY=software/stampy-1.0.32/
 SAMTOOLS=software/samtools-1.7/
 
@@ -26,24 +25,25 @@ do
                 gzip -f ${GUID}.fq
         fi
 
-        VCFFILE=${GUID}.bowtie.vcf.gz
+        VCFFILE=${GUID}.stampy.vcf.gz
         if [ -f $VCFFILE ]; then
 		echo 'vcf.gz output present'
 	else
 	        echo 'Mapping'
-	
-        	../../${BOWTIE}bowtie ../../mapto/tb_bowtie ${GUID}.fq.gz bowtie_output.sam -S
-	        ../../${SAMTOOLS}samtools view -t ../../../testdata/samtools_tab_file.txt  -o bowtie_output.bam -b -S bowtie_output.sam
-	        rm -f bowtie_output.sam
-	        ../../${SAMTOOLS}samtools sort bowtie_output.bam -t sort_tmpfile -o sorted_bowtie_output.bam -m 8G
-        	rm -f bowtie_output.bam
-        	../../${SAMTOOLS}samtools index sorted_bowtie_output.bam
+                # single-end mapping with Stampy
+        	../../${STAMPY}stampy.py -g ../../mapto/tb_stampy -h ../../mapto/tb_stampy -M ${GUID}.fq.gz > stampy_output.sam 
+                
+	        ../../${SAMTOOLS}samtools view -t ../../../testdata/samtools_tab_file.txt  -o stampy_output.bam -b -S stampy_output.sam
+	        rm -f stampy_output.sam
+	        ../../${SAMTOOLS}samtools sort stampy_output.bam -t sort_tmpfile -o sorted_stampy_output.bam -m 8G
+        	rm -f stampy_output.bam
+        	../../${SAMTOOLS}samtools index sorted_stampy_output.bam
 
         	# can compute depth from INFO/AD tag
         	echo 'Variant calling'
-	        ../../${SAMTOOLS}samtools mpileup -A -B -f ${REFFASTA} -Q25 -q30 -o40 -e20 -h100 -m2 -F0.002  -t INFO/AD -s -v  sorted_bowtie_output.bam -o $VCFFILE
-        	rm -f sorted_bowtie_output.bam
-       	 	rm -f sorted_bowtie_output.bam.bai
+	        ../../${SAMTOOLS}samtools mpileup -A -B -f ${REFFASTA} -Q25 -q30 -o40 -e20 -h100 -m2 -F0.002  -t INFO/AD -s -v  sorted_stampy_output.bam -o $VCFFILE
+        	rm -f sorted_stampy_output.bam
+       	 	rm -f sorted_stampy_output.bam.bai
 
 	fi
     else
